d29bc0520850c20f26d07f8b9d8bb00b20a3baa6
muhammad.majed404@gmail.com <muhammad.majed404@gmail.com>
2021-03-01 20:47:06 +0200

    test call api

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular';
+  users: any;
+
+  constructor(private _http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(){
+    this._http.get("https://localhost:44302/api/Users").subscribe(response => {
+      this.users = response;
+    });
+  }
+
 }
