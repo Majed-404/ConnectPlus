7e7b970364580e3c49cd5401d253d2c91d2c9c47
muhammad.majed404@gmail.com <muhammad.majed404@gmail.com>
2021-03-01 18:44:22 +0200

    create api project

---
+++ API/API/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var _users = await _context.Users.ToListAsync();
+
+            return _users;
+        }
+
+        // api/users/2
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int _id)
+        {
+            var _user = await _context.Users.FindAsync(_id);
+
+            return _user;
+        }
+    }
+}
